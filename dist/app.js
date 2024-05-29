d8513b1ac51871d2aedd8e492d8c59bd7a589a0e
Manik <tonysins43@gmail.com>
2024-05-29 10:33:52 +0600

    some changes in readme file

--- dist/app.js
+++ dist/app.js
@@ -32,7 +32,10 @@ const routes_1 = __importDefault(require("./app/routes"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+app.use((0, cors_1.default)({
+    origin: "https://flat-share-client-three.vercel.app",
+    credentials: true,
+}));
 // parser ----------------------------------------------------------------
 // make some changes
 app.use(express_1.default.json());
